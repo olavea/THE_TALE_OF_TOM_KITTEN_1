2ce4e41712560d8bf4b9daf74248a1c44bee4c46
Ola Vea <ola@olavea.com>
2019-02-10 09:43:22 +0100

    cha-cha-chaaanges!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,70 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import Bio from "../components/bio";
+import Layout from "../components/layout";
+import Image from "../components/image";
+
+import SEO from "../components/seo";
+import { rhythm } from "../utils/typography";
 
 class BlogIndex extends React.Component {
   render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
+    const { data } = this.props;
+    const siteTitle = data.site.siteMetadata.title;
+    const posts = data.allMarkdownRemark.edges;
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="All posts"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      <>
+        <Layout location={this.props.location} title={siteTitle}>
+          <div>
+            <div
+              style={{
+                maxWidth: `333px`,
+                padding: `0px`,
+                marginBottom: `0.33rem`
+              }}
+            >
+              <Image />
             </div>
-          )
-        })}
-      </Layout>
-    )
+            <SEO
+              title="All posts"
+              keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+            />
+
+            {posts.map(({ node }) => {
+              const title = node.frontmatter.title || node.fields.slug;
+              return (
+                <div>
+                  <div>
+                    <div key={node.fields.slug}>
+                      <h3
+                        style={{
+                          marginBottom: rhythm(1 / 4)
+                        }}
+                      >
+                        <Link
+                          style={{ boxShadow: `none` }}
+                          to={node.fields.slug}
+                        >
+                          {title}
+                        </Link>
+                      </h3>
+                      <small>{node.frontmatter.date}</small>
+                      <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </Layout>
+      </>
+    );
   }
 }
 
-export default BlogIndex
+export default BlogIndex;
 
 export const pageQuery = graphql`
   query {
@@ -51,7 +73,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: ASC }) {
       edges {
         node {
           excerpt
@@ -66,4 +88,6 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
+
+// on the top, put in a fast svg fluid image of book cover
